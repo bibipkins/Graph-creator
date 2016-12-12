3b4e75da3fe70c0fd998a666909bf8928e4ed09d
bibipkins <bibipkins@gmail.com>
2016-12-12 10:36:09 -0800

    Fixed crash when document is already opened

--- GraphDM/Graph.View/MainWindow.xaml.cs
+++ GraphDM/Graph.View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Novacode;
+using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -30,50 +31,64 @@ namespace GraphView
 
         private void btnTakePicture_Click(object sender, RoutedEventArgs e)
         {
+            Bitmap graphImage = GetGraphImage();
+            bool imageAdded = AddGraphImageToDoc("Graph output.docx", graphImage);
+
+            if (!imageAdded)
+            {
+                MessageBox.Show("Неможливо додати знімок графа");
+            }
+        }
+
+        private Bitmap GetGraphImage()
+        {
             System.Windows.Point screenCoords = canvas.PointToScreen(new System.Windows.Point(0, 0));
             var rect = new System.Drawing.Rectangle((int)screenCoords.X, (int)screenCoords.Y, (int)canvas.ActualWidth, (int)canvas.ActualHeight);
             Bitmap bmp = new Bitmap(rect.Width, rect.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             Graphics g = Graphics.FromImage(bmp);
             g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
 
-            //try
-            //{
-            //    bmp.Save("image.jpeg", ImageFormat.Jpeg);
-            //}
-            //catch (ExternalException exception)
-            //{
-            //    System.Windows.MessageBox.Show("Invalid file path", "Error");
-            //}
+            return bmp;
+        }
 
-            string fileName = "Graph output.docx";
-
+        private bool AddGraphImageToDoc(string fileName, Bitmap bmp)
+        {
             using (MemoryStream ms = new MemoryStream())
             {
                 bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
                 ms.Seek(0, SeekOrigin.Begin);
 
-                if (System.IO.File.Exists(fileName))
-                    using (DocX doc = DocX.Load(fileName))
-                    {
-                        Novacode.Image img = doc.AddImage(ms);
-                        Paragraph p = doc.InsertParagraph();
-                        p.Alignment = Alignment.center;
-                        p.Append("\n");
-                        Picture pic1 = img.CreatePicture();
-                        p.InsertPicture(pic1, 0);
-                        doc.Save();
-                    }
-                else
-                    using (DocX doc = DocX.Create(fileName))
-                    {
-                        Novacode.Image img = doc.AddImage(ms); // Create image.
-                        Paragraph p = doc.InsertParagraph();
-                        p.Alignment = Alignment.center;
-                        p.Append("\n");
-                        Picture pic1 = img.CreatePicture();
-                        p.InsertPicture(pic1, 0);
-                        doc.Save();
-                    }
+                try
+                {
+                    if (System.IO.File.Exists(fileName))
+                        using (DocX doc = DocX.Load(fileName))
+                        {
+                            Novacode.Image img = doc.AddImage(ms);
+                            Paragraph p = doc.InsertParagraph();
+                            p.Alignment = Alignment.center;
+                            p.Append("\n");
+                            Picture pic1 = img.CreatePicture();
+                            p.InsertPicture(pic1, 0);
+                            doc.Save();
+                        }
+                    else
+                        using (DocX doc = DocX.Create(fileName))
+                        {
+                            Novacode.Image img = doc.AddImage(ms); // Create image.
+                            Paragraph p = doc.InsertParagraph();
+                            p.Alignment = Alignment.center;
+                            p.Append("\n");
+                            Picture pic1 = img.CreatePicture();
+                            p.InsertPicture(pic1, 0);
+                            doc.Save();
+                        }
+                }
+                catch(Exception e)
+                {
+                    return false;
+                }
+
+                return true;
             }
         }
     }
